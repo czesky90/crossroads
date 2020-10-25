2e2c2f9f0b9decdf072f781ab3e2ae6a682bc6e4
czesky90 <gjstaszczak@gmail.com>
2020-10-25 20:33:36 +0100

    Add checking if dictionary lists have overlapping data, create a page showing some problems, code refactor + gitignore update
    Added features:
    lists_have_common_parts - checks that the dates in both dictionary lists have are common parts,
    consolation_coordinates - if the lists do not coincide but the closest dates are less than 7 days apart returns two new lists that contain only those dates,
    establishing_common_part - checks which page and where exactly to trim the lists to get only common parts,
    list_shortener - trims the list at the selected point (based on the above function).
    
    Updated features and refactoring:
    locations_page - calls the newly created page template "problems.html" and displays problem message,
    generate_coordinates - the function has been cleaned of unnecessary calls to other functions (less spaghetti code).
    
    Additionally, new test files have been added - sample1_not_match.json and sample2_not_match.json.

--- src/tests/tests.py
+++ src/tests/tests.py
@@ -1,21 +1,38 @@
 import unittest
 import os
+import datetime
 from unittest.mock import patch, MagicMock
-from crossroads import main
+from src.crossroads import main
 
 class TestCrossRoads(unittest.TestCase):
     tests_dir = os.path.dirname(__file__)
-    correct_test_data1 = open(os.path.join(tests_dir, "sample.json"), "rb").read()
+    correct_test_data1 = open(os.path.join(tests_dir, "sample1.json"), "rb").read()
     correct_test_data2 = open(os.path.join(tests_dir, "sample2.json"), "rb").read()
 
     def test_generate_coordinates(self):
-        """ Test generated cooridnates """
-        #correct_coordinates = None
+        """ Test generated coordinates """
+
+        correct_coordinates = [{'latitude': 51.207902, 'longitude': 16.16104, 'timestamp': datetime.datetime(2012, 11, 3, 17, 19, 26, 356000), 'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'},
+            {'latitude': 51.2078704, 'longitude': 16.1610474, 'timestamp': datetime.datetime(2012, 11, 3, 17, 20, 26, 438000), 'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'},
+            {'latitude': 51.2079648, 'longitude': 16.160865, 'timestamp': datetime.datetime(2013, 9, 28, 11, 28, 25, 688000), 'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'},
+            {'latitude': 51.2079121, 'longitude': 16.1610401, 'timestamp': datetime.datetime(2012, 11, 3, 17, 19, 26, 356000), 'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'},
+            {'latitude': 51.2078805, 'longitude': 16.1610475, 'timestamp': datetime.datetime(2012, 11, 3, 17, 20, 26, 438000), 'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'},
+            {'latitude': 51.2079749, 'longitude': 16.1608651, 'timestamp': datetime.datetime(2013, 9, 28, 11, 28, 25, 688000), 'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'}]
+
         coordinates = main.generate_coordinates(self.correct_test_data1, self.correct_test_data2)
+
+        count = 0
         for coordinate in coordinates:
             self.assertTrue(isinstance(coordinate, main.Coordinates))
-            print(coordinate.__repr__())
-        #assertEqual(coordinates, correct_coordinates)
+
+            self.assertEqual(coordinate.__repr__(), str(correct_coordinates[count]))
+            count += 1
+
+class Test(unittest.TestCase):
+    def data_which_overlap(self):
+        """ Test json parser """
+
+
 
 if __name__ == '__main__':
     unittest.main()
